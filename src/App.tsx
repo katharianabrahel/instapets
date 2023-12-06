0d77f8c16873d3d1f9a9d9b265fb4203411a2dd4
Marcos Antonio <marcosantonio-10@live.com>
2023-12-06 09:17:20 -0300

    feat: front done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [login, setLogin] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleChangeLogin = (e: ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  };
+
+  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(login);
+  }, [login, password]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Aqui você pode adicionar a lógica para autenticar o usuário, como enviar os dados para o backend
+    console.log("Username:", login);
+    console.log("Password:", password);
+    // Reset dos campos após a submissão
+    setLogin("");
+    setPassword("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="login-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Login"
+            value={login}
+            onChange={handleChangeLogin}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={handleChangePassword}
+          />
+          <input type="submit" value={"Entrar"} />
+        </form>
+        <a href="/signup">Criar conta</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
